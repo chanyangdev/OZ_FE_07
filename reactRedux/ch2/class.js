1d5cfecc80f0bda71d568b6b077992ceb49e9ad5
chanyangpraise <cychoi710@icloud.com>
2024-11-08 12:00:42 +0900

    ✨ Created hamburger 🍔
    Created hamburger to review react component and JSX

--- reactRedux/ch2/class.js
+++ reactRedux/ch2/class.js
@@ -1,14 +1,20 @@
 class person {
-  constructor(name, age, gender) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  } 
-
-  introduce() {
-    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender}`)
+  constructor(name, age) {
+    this.name = name
+    this.age = age
   }
 }
 
+const p = new person('홍길동', 25)
+console.log(p)
 
+class student extends person {
+  constructor(name, age, major) {
+    super(name, age)
+    this.major = major
+  }
+}
+
+const s = new student('홍길동', 25, '컴퓨터공학')
+console.log(s)
 
--- reactRedux/vite-project/src/App.jsx
+++ reactRedux/vite-project/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+return ( <> 
+<Bread />
+<Patty />
+<Lettuce />
+<Tomato />
+<Cheese />
+<Bread />
+  </>
+  );
+}
 
+function Bread () {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className="bread">
+  밀가루
+  <span> 물</span>
+  <span> 계란</span>
+  <span> 이스트</span>
+</div>
   )
+}
+
+function Patty () {
+  return (
+  <div className="patty">
+  소고기
+  <span> 후추</span>
+  <span> 소금</span>
+</div>
+  )
+}
+
+
+function Lettuce () {
+  return (
+  <div className='lettace'>
+  잎사귀 
+  <span> 쥴기</span>
+</div>
+  )
+}
+
+function Tomato () {
+  return (
+  <div className='tomato'>
+  껍질
+  <span> 과육</span>
+  <span> 씨앗</span>
+</div>
+  )
+}
+
+function Cheese () {  
+    return (
+  <div className='cheese'>
+  우유 
+  <span> 소금</span>
+  <span> 색소</span>
+</div>
+    )
 }
 
 export default App
