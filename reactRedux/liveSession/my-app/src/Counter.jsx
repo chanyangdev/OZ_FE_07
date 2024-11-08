8fa5bb5e4adffab2bc688accbeaf265d4dfea9f9
chanyangpraise <cychoi710@icloud.com>
2024-11-08 19:09:08 +0900

    🎨 Changed counter to arrow function
    changed to arrow function for better readability

--- reactRedux/liveSession/my-app/src/Counter.jsx
+++ reactRedux/liveSession/my-app/src/Counter.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Counter = () => {
-  const [number, setNumber] = useState(0);
-
-  const onIncrease = () => {
-    setNumber((prev) => prev + 1);
-  };
-
-  const onDecrease = () => {
-    setNumber((prev) => prev - 1);
-  };
-
+  const [number, setNumber] = useState(10);
+  
+  const onIncrease = () => setNumber((prev) => prev + 1);
+  
+  const onDecrease = () => setNumber((prev) => prev - 1);
+  
   return (
     <div>
-      <h1>Counter</h1>
-      <h2>{number}</h2>
+      <h1>{number}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
