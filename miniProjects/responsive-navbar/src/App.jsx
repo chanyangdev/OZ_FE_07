4655064d3fded8b38aa4da249f4b6a534d702cab
chanyangpraise <cychoi710@icloud.com>
2024-11-13 15:56:38 +0900

    💄 Finished styling for nav bar
    added hover effects, logo on the left, home,producs,explore,contact page links

--- miniProjects/responsive-navbar/src/App.jsx
+++ miniProjects/responsive-navbar/src/App.jsx
@@ -1,5 +1,28 @@
+import logo from "./assets/Tailwind_CSS_logo.svg.png"
 function App() {
-  
-}
 
+  return (
+    <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400">
+      <header className="flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md">
+        <a href="#">
+          <img src={logo} alt="" className="w-52 hover:scale-105 transition-all"/>
+        </a>
+
+        <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+          <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Home</li>
+          <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Products</li>
+          <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Explore</li>
+          <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Contact</li>
+        </ul>
+
+        <div className="relative hidden md:flex items-center justify-center gap-3">
+          <i className="bx bx-search absolute left-3 text-2xl text-gray-500"></i>
+          <input type="text" placeholder="Search..." className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500"/>
+        </div>
+
+        <i className="bx bx-menu xl:hidden block text-5xl cursor-pointer"></i>
+      </header>
+    </div>
+  )
+}
 export default App
